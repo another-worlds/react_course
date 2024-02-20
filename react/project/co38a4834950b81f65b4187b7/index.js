5882bc77b525c98db25af88e8e1018cf369198e0
another-worlds <a.egorshev@gmail.com>
2024-02-20 20:56:37 +0600

    added projects

---
+++ react/project/co38a4834950b81f65b4187b7/index.js
@@ -0,0 +1,8 @@
+var ReactDOM = require('react-dom');
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+html = `<h1>12</h1>`
+ReactDOM.render(html, document.getElementById("root"));
